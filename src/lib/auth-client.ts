84db6fe2b6f617f523259916f2b14f419c10c005
Subhojit Ghosh <subhojitghosh064@gmail.com>
2026-03-04 01:53:07 +0530

    feat: add better-auth client with organization and 2FA plugins

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,22 @@
+"use client";
+
+import { createAuthClient } from "better-auth/client";
+import { organizationClient } from "better-auth/client/plugins";
+import { twoFactorClient } from "better-auth/client/plugins";
+
+export const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL ?? "http://localhost:3000",
+
+  plugins: [
+    organizationClient(),
+
+    twoFactorClient({
+      onTwoFactorRedirect() {
+        window.location.href = "/auth/2fa";
+      },
+    }),
+  ],
+});
+
+// Named exports for convenience
+export const { signIn, signOut, signUp, useSession } = authClient;
